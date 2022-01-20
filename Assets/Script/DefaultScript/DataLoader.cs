60c54c0bad8b61461fe6631d6968706fa50b9363
Telepic <596248470@qq.com>
2022-01-20 23:52:54 +0800

    更新了城市数据，增加了城市经济数据和旅游数据，下一步将会增加各种事件和数据交互

--- Assets/Script/DefaultScript/DataLoader.cs
+++ Assets/Script/DefaultScript/DataLoader.cs
@@ -22,15 +22,18 @@ public class DataLoader : MonoBehaviour
         var buttonParent = GameObject.Find("Buttons");
         foreach (var line in Cities_Data)//按行读取，每一行是一个城市的各项数据
         {
+            int id = System.Convert.ToInt32(line[0]);//id
             float latitude = System.Convert.ToSingle(line[3]); //纬度
             float longitude = System.Convert.ToSingle(line[4]); //经度
+            float economy = System.Convert.ToSingle(line[7]); //经济指数
+            float tourism = System.Convert.ToSingle(line[8]); //旅游指数
             //旋转坐标计算（垂直于法向量的角度分量）
             float ry = longitude - 90;
             float rx = latitude;
             float rz = 0;
             latitude = latitude / 180 * System.Convert.ToSingle(3.14159265); //转换成弧度
             longitude = longitude / 180 * System.Convert.ToSingle(3.14159265); //转换成弧度
-            City temp = new City(line[6], latitude, longitude); 
+            City temp = new City(id, line[6], latitude, longitude, economy, tourism); 
             Debug.Log(line[2]);
             cManager.add(temp);
             //法向量（相对于球心的向量）
--- Assets/Script/DefaultScript/Managers/CitiesManager.cs
+++ Assets/Script/DefaultScript/Managers/CitiesManager.cs
@@ -4,39 +4,30 @@ using UnityEngine;
 
 public class City
 { //单个城市的各项数据
-    private string name;
-    private float x;
-    private float y;
-    private string city_introduction;
-    public City(string name, float x, float y)
+    public int id;//编号
+    public string name;//城市名
+    public float x;//纬度
+    public float y;//经度
+    public string city_introduction;//城市介绍
+    public float economy;//经济指数
+    public float tourism;//旅游指数
+
+    public City(int id, string name, float x, float y, float economy, float tourim)
     {
+        this.id = id;
         this.name = name;
         this.x = x;
         this.y = y;
+        this.economy = economy;
+        this.tourism = tourism;
     }
     public string getName()
     {
         return this.name;
     }
-    float getX()
-    {
-        return x;
-    }
-    float getY()
-    {
-        return y;
-    }
-    public static void GetDistance(City c1, City c2)
+    public static void GetDistance(City c1, City c2)//计算两点间距离
     {
 
-    }
-    public static void GetTicketIn(City c1,City c2)
-    {
-
-    }
-    public string getIntroduction()
-    {
-        return this.city_introduction;
     }
 }
 
