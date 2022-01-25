7b7a4847ecdf3ab4ead68dc9700329fbf7910711
“迁日月” <2518799522@qq.com>
2022-01-25 21:16:32 +0800

    添加了rank界面

---
+++ Assets/Script/RankScript/Ranking.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ranking : MonoBehaviour
+{
+    public GameObject Lines;
+    public GameObject oneline;
+    public Quaternion b = new Quaternion(0, 0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < 20; i++)
+        {
+            Vector3 a = new Vector3(180,(270-90*i), 0);
+            GameObject l=Instantiate(oneline, a, b, Lines.transform);
+            l.name = "line"+i.ToString() ;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
