388ef4ffbc5ab1cc2b7b82dfaf9a269a4c3946cb
F-zg <1301870842@qq.com>
2022-03-23 13:20:02 +0800

    背包管理器

--- Assets/Script/DefaultScript/Managers/BagManager.cs
+++ Assets/Script/DefaultScript/Managers/BagManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BagManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Bag
     {
-        
+        public string username;
+        Dictionary<int, int> planeBag = new Dictionary<int, int>();
+        //判断背包是否有该id的飞机
+        public bool IsPlane(int id)
+        {
+            if (this.planeBag.ContainsKey(id))
+                return true;
+            else
+                return false;
+        }
+        //输出背包中该id飞机的数量
+        public int GetNum(int id)
+        {
+            foreach(KeyValuePair<int,int>bag in this.planeBag )
+            {
+                if (bag.Key == id)
+                    return bag.Value;
+            }
+            return 0;
+        }
+        public string getUsername()
+        {
+            return this.username;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public class BagsManager
     {
-        
+        List<Bag> Bags;
+        public BagsManager()
+        {
+            this.Bags = new List<Bag>();
+        }
+        public Bag GetBagbyUsername(string username)
+        {
+            foreach(var b in Bags)
+            {
+                if(b.getUsername().Equals(username))
+                {
+                    return b;
+                }
+            }
+            return null;
+        }
+        public void add (Bag b)
+        {
+            this.Bags.Add(b);
+        }
     }
 }
