dfc50a1f4fb04d28444f5c5fb62ee4721f9f6806
Telepic <596248470@qq.com>
2022-03-26 16:31:25 +0800

    添加研究界面按钮图标

--- Assets/Script/DefaultScript/Managers/ResearchManager.cs
+++ Assets/Script/DefaultScript/Managers/ResearchManager.cs
@@ -2,9 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResearchManager : MonoBehaviour
+public enum ResearchType
 {
-    // Start is called before the first frame update
+    SERVICE,
+    TECHONOLOGY,
+    INVESTMENT,
+    MANAGEMENT
+}
+
+public class Research
+{
+    public ResearchType Type; //研究的类别
+    public string UID; //每项研究的独立ID
+    public int cost;//金钱花费
+    public List<Research> Pre_researches;//前置研究
+    bool Researched;
+
+}
+
+public class ResearchManager
+{
+    
     void Start()
     {
         
---
+++ Assets/Script/Entities/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    //this script is applicated in mutiplayer mode.
+    public string name;
+
+}
