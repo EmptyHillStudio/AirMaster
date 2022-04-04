ce23746a8250bf70492cfdd846310f181a1c3ba7
F-zg <1301870842@qq.com>
2022-04-04 23:47:48 +0800

    选择飞机界面框架

---
+++ Assets/Script/plane/ShowAllPlane.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAllPlane : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (KeyValuePair<Plane, int> kvp in GlobalVariable.planeDic)
+        {
+
+        }
+    }
+    
+    
+}
